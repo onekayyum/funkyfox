1d8668e8711db0259fc9aa5775e8597d3979abab
kayyum khan <152464768+onekayyum@users.noreply.github.com>
2024-02-01 16:06:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,18 +5,18 @@ from logging.handlers import RotatingFileHandler
 
 # ---------------------- ᴄᴏɴғɪɢ ---------------------- #
 
-API_ID = int(getenv("API_ID", "18719789"))
-API_HASH = getenv("API_HASH", "a03c27be3e14aac40f62cb4e95207fae")
-BOT_TOKEN = getenv("BOT_TOKEN", "6056505972:AAG-5upMP7qhU9Vpd8gMwQgkeMPoQBcn2dw")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1001914497289"))
-OWNER_ID = list(map(int, getenv("SUDO_USERS", "6109551937 5416887843 6018550523").split()))
-DB_URI = getenv("DATABASE_URL", "postgres://rtzsspzt:DLuJvx8TQGqYqt0XKR9CDnLZmRs6UMr3@berry.db.elephantsql.com/rtzsspzt")
-FORCE_SUB_CHANNEL = int(getenv("FORCE_SUB_CHANNEL", "-1001831916389"))
+API_ID = int(getenv("API_ID", "22517234"))
+API_HASH = getenv("API_HASH", "a13f39f3effcd7618d2a61481f3c2392")
+BOT_TOKEN = getenv("BOT_TOKEN", "6560314978:AAH6mpBnhgyNw-1xs8D1kCgMLpckKRRwav4")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1001909805668"))
+OWNER_ID = list(map(int, getenv("SUDO_USERS", "6733071568 6867128113").split()))
+DB_URI = getenv("DATABASE_URL", "mongodb+srv://raajkundra:Kayamkhani@rajkundra.jsuih8v.mongodb.net/?retryWrites=true&w=majority")
+FORCE_SUB_CHANNEL = int(getenv("FORCE_SUB_CHANNEL", "-1001909805668"))
 BOT_WORKERS = int(getenv("BOT_WORKERS", "5"))
 
 # --------- ʟɪɴᴋ sʜᴏʀᴛɴᴇʀ ᴄᴏɴᴠᴇʀᴛᴇʀ ---------
-URL_SHORTNER_API = getenv("URL_SHORTNER_API", "http://urlshortx.com/api?api")
-URL_SHORTNER_API_KEY = getenv("URL_SHORTNER_API_KEY", "c460f2f1a0f68b2c56cda7f12121c210ed10b08b")
+URL_SHORTNER_API = getenv("URL_SHORTNER_API", "https://publicearn.com/kayyumkhan")
+URL_SHORTNER_API_KEY = getenv("URL_SHORTNER_API_KEY", "ffe90fdeaa90e3518ad52debe75d4d493adb2b90")
 
 
 default_custom_caption = """
